116d3c057bd92c63b4ea422c574e5ed42a15ca22
hotchkjo <hotchkjo@oregonstate.edu>
2025-12-28 19:19:51 -0800

    Update stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
-    "indentation": 2,
     "no-duplicate-selectors": true,
     "color-no-invalid-hex": true,
-    "comment-empty-line-before": "never"
+    "indentation": null,
+    "comment-empty-line-before": null
   }
 };
