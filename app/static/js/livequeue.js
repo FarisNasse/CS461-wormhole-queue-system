fb0efbbf8c4c322be523973694082995c96bea20
GrahamAtOSU <glaznerg@oregonstate.edu>
2026-01-21 15:20:34 -0800

    changes 'on connect' and 'on new ticket' functions to call new updateTicketTable()

--- app/static/js/livequeue.js
+++ app/static/js/livequeue.js
@@ -4,11 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     socket.on('connect', function() {
         console.log('Connected to queue namespace');
+        updateTicketTable();
+        console.log('Initial ticket table loaded');
     });
 
     socket.on('new_ticket', function(data) {
         console.log('New ticket created:', data);
-        refreshLiveQueue();
+        updateTicketTable();
     });
 
     socket.on('queue_refresh', function(data) {
@@ -22,5 +24,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function refreshLiveQueue() {
         location.reload();
+    }
+
+    function updateTicketTable() {
+        // Implementation for addding tickets dynamically
+        fetch('/api/opentickets')
+            .then(response => {
+                console.log('Response status', response.status);
+                return response.json()
+            })
+            .then(tickets => {
+                console.log('API tickets:', tickets); // Check structure
+
+                const ticketTableBody = document.querySelector('#tickets tbody');
+                ticketTableBody.innerHTML = ''; // Clear existing rows
+                tickets.forEach((ticket, index) => {
+                    const row = document.createElement('tr');
+                    row.id = `ticket-${ticket.id}`;
+                    row.innerHTML = `
+                        <td>${index + 1}</td>
+                        <td>${ticket.student_name}</td>
+                        <td>${ticket.table}</td>
+                        <td>${ticket.physics_course}</td>
+                    `;
+                    ticketTableBody.appendChild(row);
+                });
+                //document.getElementById('ticket-count').textContent = tickets.length;
+                document.getElementById('refresh-time').textContent = new Date().toLocaleTimeString();
+            })
+            .catch(error => console.error('Error fetching tickets:', error));
     }
 });
