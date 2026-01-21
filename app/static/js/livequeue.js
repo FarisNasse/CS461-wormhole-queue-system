385e5c640df395f351bc7ef9866c85f8f95538d3
GrahamAtOSU <glaznerg@oregonstate.edu>
2026-01-21 14:32:29 -0800

    userpage.js waits until page is loaded

--- app/static/js/livequeue.js
+++ app/static/js/livequeue.js
@@ -1,24 +1,26 @@
-document
-const socket = io('/queue');
+document.addEventListener('DOMContentLoaded', function() {
+    // Connect to the queue namespace
+    const socket = io('/queue');
 
-socket.on('connect', function() {
-    console.log('Connected to queue namespace');
+    socket.on('connect', function() {
+        console.log('Connected to queue namespace');
+    });
+
+    socket.on('new_ticket', function(data) {
+        console.log('New ticket created:', data);
+        refreshLiveQueue();
+    });
+
+    socket.on('queue_refresh', function(data) {
+        console.log('Queue refresh event received');
+        refreshLiveQueue();
+    });
+
+    socket.on('disconnect', function() {
+        console.log('Disconnected from queue namespace');
+    });
+
+    function refreshLiveQueue() {
+        location.reload();
+    }
 });
-
-socket.on('new_ticket', function(data) {
-    console.log('New ticket created:', data);
-    refreshLiveQueue();
-});
-
-socket.on('queue_refresh', function(data) {
-    console.log('Queue refresh event received');
-    refreshLiveQueue();
-});
-
-socket.on('disconnect', function() {
-    console.log('Disconnected from queue namespace');
-});
-
-function refreshLiveQueue() {
-    location.reload();
-}
