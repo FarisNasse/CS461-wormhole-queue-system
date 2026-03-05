6a74569961fbbe286e90d7722bfee623cfbd183c
hotchkjo <hotchkjo@oregonstate.edu>
2026-03-04 17:02:23 -0800

    Corrected Testing Issues

--- migrations/versions/f3188ae0a966_add_is_active_field_to_user_model.py
+++ migrations/versions/f3188ae0a966_add_is_active_field_to_user_model.py
@@ -5,9 +5,8 @@ Revises: 07c58cacfb80
 Create Date: 2026-03-02 11:16:47.887751
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = 'f3188ae0a966'
