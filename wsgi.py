876dceb25606312a996c18fc3404a923fd7974cb
hotchkjo <hotchkjo@oregonstate.edu>
2026-04-17 19:15:08 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 # wsgi.py
 from app import create_app, socketio
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 if __name__ == "__main__":
     socketio.run(app)
